e01f8762fe2e99d8fb69e6a3fcedb7125355a09e
seiya <r108338@yahoo.co.jp>
2020-07-24 20:39:05 +0900

    abc-172-d-rust

---
+++ ABC/17x/172/d.rs
@@ -0,0 +1,70 @@
+use std::io::stdin;
+use std::str::FromStr;
+
+fn main(){
+    let n: usize = get_one();
+    let mut ans = 0;
+    for i in 1..n+1 {
+        ans += g(i, n);
+    }
+    println!("{}", ans);
+}
+
+fn g(i: usize, n: usize) -> usize {
+    let m = n / i;
+    let s = m * (m+1) / 2;
+    s * i
+}
+
+#[allow(dead_code)]
+fn get_line() -> String {
+    let mut s = String::new();
+    match stdin().read_line(&mut s){
+        Ok(_) => {s.trim().to_string()}
+        Err(_) => String::new()
+    }
+}
+
+#[allow(dead_code)]
+fn get_vec<T: std::str::FromStr>() -> Vec<T> {
+    let line = get_line();
+    line.split_whitespace().filter_map(|x| x.parse().ok()).collect()
+}
+
+#[allow(dead_code)]
+fn get_one<T: FromStr + Copy>() -> T {
+    let v = get_vec();
+    v[0]
+}
+
+#[allow(dead_code)]
+fn get_pair<T: FromStr + Copy>() -> (T, T) {
+    let v = get_vec();
+    (v[0], v[1])
+}
+
+#[allow(dead_code)]
+fn get_triple<T: FromStr + Copy>() -> (T, T, T) {
+    let v = get_vec();
+    (v[0], v[1], v[2])
+}
+
+#[allow(dead_code)]
+fn get_chars() -> Vec<char> {
+    get_line().chars().collect()
+}
+
+#[allow(dead_code)]
+fn vec_min(xs: &Vec<i64>) -> i64 {
+    xs.iter().map(|&x|x).fold(std::i64::MAX, std::cmp::min)
+}
+
+#[allow(dead_code)]
+fn vec_max(xs: &Vec<i64>) -> i64 {
+    xs.iter().map(|&x|x).fold(std::i64::MIN, std::cmp::max)
+}
+
+#[allow(dead_code)]
+fn vec_sum(xs: &Vec<i64>) -> i64 {
+    xs.iter().fold(0, |acc, &x| acc+x)
+}
