54bcfab1e946a8ca205f40b767439aaa97fe840e
seiya <20365512+seiyab@users.noreply.github.com>
2024-06-29 17:21:57 +0900

    Revert "approach 2"
    This reverts commit 5a57bcaff81fa376f61ea3bd8f43e3787fe1f9ad.

--- AHC/03x/032/main.rs
+++ AHC/03x/032/main.rs
@@ -1,13 +1,9 @@
 use std::io::stdin;
 use std::str::FromStr;
-use rand::Rng;
-use std::time::{Duration, SystemTime};
 
 const MOD: i64 = 998244353;
 
 fn main() {
-    let start = SystemTime::now();
-    let mut rng = rand::thread_rng();
     let (n, m, k): (usize, usize, usize) = get_triple();
     let mut A = get_square(n);
     let mut Ss = Vec::new();
@@ -16,24 +12,22 @@ fn main() {
     }
     
     let mut env = Env::new(n, m, k, A.clone(), Ss.clone());
-    
-    loop {
-        let el = start.elapsed().map(|e| e.as_millis()).unwrap_or(2_000);
-        if el > 1_900 {
-            break;
-        }
-        let t: usize = rng.gen_range(0..m);
-        let z: usize = rng.gen_range(0..k);
-        let p: usize = rng.gen_range(0..n - 2);
-        let q: usize = rng.gen_range(0..n - 2);
-        let step = step(t, p, q);
-        let d = env.dry_patch(z, &step);
-        let th: i64 = rng.gen_range(0..=(MOD * (1900 - el as i64) / 1900) / 3);
-        if d + th > 0 {
-            env.patch(z, Some(step));
+    for i in 0..(n-2) {
+        for j in 0..(n-2) {
+            let mut c = None;
+            let mut l = env.A[i][j];
+            for t in 0..m {
+                let s = (env.A[i][j] + Ss[t][0][0]) % MOD;
+                if s > l {
+                    l = s;
+                    c = Some(step(t, i, j));
+                }
+            }
+            if let Some(step) = c {
+                env.apply(&step);
+            }
         }
     }
-
     env.print();
 }
 
@@ -43,12 +37,12 @@ struct Env {
     k: usize,
     A: Vec<Vec<i64>>,
     Ss: Vec<Vec<Vec<i64>>>,
-    actions: Vec<Option<Step>>,
+    actions: Vec<Step>,
 }
 
 impl Env {
     fn new(n: usize, m: usize, k: usize, A: Vec<Vec<i64>>, Ss: Vec<Vec<Vec<i64>>>) -> Self {
-        Env {n, m, k, A, Ss, actions: vec![None; k]}
+        Env {n, m, k, A, Ss, actions: Vec::new()}
     }
     
     fn score(&self) -> i64 {
@@ -68,7 +62,7 @@ impl Env {
                 let mut x = self.A[i][j];
                 if i <= step.p && step.p < i + 3 
                     && j <= step.q && step.q < j + 3 {
-                        x += self.Ss[step.t][i][j]
+                        x += self.Ss[step.t][step.p + i][step.q + j]
                 }
                 s += x % MOD;
             }
@@ -76,57 +70,18 @@ impl Env {
         return s;
     }
     
-    fn patch(&mut self, t: usize, step: Option<Step>) {
-        let prev = self.actions[t].clone();
-        if let Some(ref s) = step {
-            for i in 0..3 {
-                for j in 0..3 {
-                    let inc = self.Ss[s.t][i][j];
-                    self.A[s.p + i][s.q + j] += inc;
-                    self.A[s.p + i][s.q + j] %= MOD;
-                }
-            }
-        }
-        if let Some(s) = prev {
-            for i in 0..3 {
-                for j in 0..3 {
-                    let dec = MOD - self.Ss[s.t][i][j];
-                    self.A[s.p + i][s.q + j] += dec;
-                    self.A[s.p + i][s.q + j] %= MOD;
-                }
-            }
-        }
-        self.actions[t] = step;
-    }
-    
-    fn dry_patch(&mut self, t: usize, step: &Step) -> i64 {
-        let prev = self.actions[t].clone();
-        let mut diff = 0;
+    fn apply(&mut self, step: &Step) {
+        self.actions.push(step.clone());
         for i in 0..3 {
             for j in 0..3 {
-                let inc = self.Ss[step.t][i][j];
-                let prevA = self.A[step.p + i][step.q + j];
-                let newA = (prevA + inc) % MOD;
-                diff += newA - prevA;
+                self.A[step.p + i][step.q + j] = (self.A[step.p + i][step.q + j] + self.Ss[step.t][i][j]) % MOD;
             }
         }
-        if let Some(s) = prev {
-            for i in 0..3 {
-                for j in 0..3 {
-                    let dec = MOD - self.Ss[s.t][i][j];
-                    let prevA = self.A[s.p + i][s.q + j];
-                    let newA = (prevA + dec) % MOD;
-                    diff += newA - prevA;
-                }
-            }
-        }
-        return diff;
     }
     
     fn print(&self) {
-        let xs = self.actions.iter().filter(|a| a.is_some()).map(|a| a.clone().unwrap()).collect::<Vec<Step>>();
-        println!("{}", xs.len());
-        for a in xs.iter() {
+        println!("{}", self.actions.len());
+        for a in self.actions.iter() {
             println!("{} {} {}", a.t, a.p, a.q);
         }
     }
