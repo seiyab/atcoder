205252bd4ebde6fcb82bb2fee06b2cbc9a16f7ad
seiya <20365512+seiyab@users.noreply.github.com>
2024-08-05 13:19:20 +0900

    ahc033 split by column

--- AHC/03x/033/main.rs
+++ AHC/03x/033/main.rs
@@ -6,20 +6,87 @@ fn main() {
     let n: usize = get_one();
     let a: Vec<Vec<usize>> = get_square(n);
     let mut s = State::new(n, &a);
-    s.post_step();
-    s.step_big(&Operation::Nop);
-    s.post_step();
-    let mut os = Vec::new();
-    os.push(Operation::Nop);
+    let mut os_big = Vec::new();
+    let mut os_center = Vec::new();
+    let mut os_right = Vec::new();
     
-    loop {
+    for _ in 0..3_000 {
+        s.post_step();
         if s.expected.iter().all(|e| *e == None) {
             break;
         }
+        
+        if s.big_crane.target == None && s.big_crane.grabbing == None {
+            s.big_crane.target = target_big(&s);
+        }
+        let cc = s.small_cranes[0].unwrap();
+        if cc.target == None &&
+            cc.x == 2 &&
+            cc.grabbing == None
+        {
+            let t = target_center(&s);
+            if let Some(tc) = s.small_cranes[0].as_mut() {
+                tc.target = t;
+            }
+        }
+        let rc = s.small_cranes[1].unwrap();
+        if rc.target == None &&
+            rc.x == 4 &&
+            rc.grabbing == None
+        {
+            let t = target_right(&s);
+            if let Some(rc) = s.small_cranes[1].as_mut() {
+                rc.target = t;
+            }
+        }
+        
+        let bo = op_big(&s);
+        let co = op_center(&s);
+        let ro = op_right(&s);
+        
+        if bo == Operation::Nop && co == Operation::Nop && ro == Operation::Nop {
+            // panic!("stopped");
+            break;
+        }
+        
+        os_big.push(bo);
+        os_center.push(co);
+        os_right.push(ro);
+        s.step(&vec![bo, co, ro, Operation::Nop, Operation::Nop]);
+    }
+    if s.expected.iter().any(|e| *e != None) {
+        s.post_step();
+        if s.small_cranes[0].unwrap().grabbing != None || s.small_cranes[1].unwrap().grabbing != None {
+            os_big.push(Operation::Nop);
+            let oc = if s.small_cranes[0].unwrap().grabbing != None {
+                Operation::Release
+            } else {
+                Operation::Nop
+            };
+            os_center.push(oc);
+            let or = if s.small_cranes[1].unwrap().grabbing != None {
+                Operation::Release
+            } else {
+                Operation::Nop
+            };
+            os_right.push(or);
+            s.step(&vec![Operation::Nop, oc, or, Operation::Nop, Operation::Nop]);
+            s.post_step();
+        }
+        os_big.push(Operation::Nop);
+        os_center.push(Operation::Bomb);
+        os_right.push(Operation::Bomb);
+        s.step(&vec![Operation::Nop, Operation::Bomb, Operation::Bomb, Operation::Nop, Operation::Nop]);
+    }
+    for _ in 0..1_000 {
+        if s.expected.iter().all(|e| *e == None) {
+            break;
+        }
+        s.post_step();
         for p in plan_pick(&s).iter() {
             s.step_big(p);
             s.post_step();
-            os.push(*p);
+            os_big.push(*p);
         }
         let g = match s.big_crane.grabbing {
             Some(g) => g,
@@ -30,26 +97,319 @@ fn main() {
                 for p in path(&s.big_crane.pos(), &(s.field[d].len()-1, d)).iter() {
                     s.step_big(p);
                     s.post_step();
-                    os.push(*p);
+                    os_big.push(*p);
                 }
                 s.step_big(&Operation::Release);
                 s.post_step();
-                os.push(Operation::Release);
+                os_big.push(Operation::Release);
             },
             None => {
                 for p in plan_put(&s).iter() {
                     s.step_big(p);
                     s.post_step();
-                    os.push(*p);
+                    os_big.push(*p);
                 }
             },
-        };
+        }
     }
 
-    println!("{}", format_ops(&os));
-    for _ in 1..s.n {
+    println!("{}", format_ops(&os_big));
+    println!("{}", format_ops(&os_center));
+    println!("{}", format_ops(&os_right));
+    for _ in 3..s.n {
         println!("B");
     }
+}
+
+fn op_big(s: &State) -> Operation {
+    let cr = s.big_crane;
+    if let Some(g) = cr.grabbing {
+        if cr.x == 1 {
+            return Operation::Release;
+        }
+        let mut d = 10;
+        let mut y = 0;
+        for i in empty_rows(s, 1).iter().copied() {
+            let dd = diff(cr.y, i);
+            if dd < d {
+                d = dd;
+                y = i;
+            }
+        }
+        if d < 10 {
+            if cr.y < y {
+                return Operation::Down;
+            } else if cr.y > y {
+                return Operation::Up;
+            } else {
+                return Operation::Right;
+            }
+        }
+        for i in empty_rows(s, 0) {
+            let dd = diff(cr.y, i);
+            if dd < d {
+                d = dd;
+                y = i;
+            }
+        }
+        if d < 10 {
+            if cr.y < y {
+                return Operation::Down;
+            } else if cr.y > y {
+                return Operation::Up;
+            } else {
+                return Operation::Release;
+            }
+        }
+        return Operation::Nop;
+    } else if cr.x > 0 {
+        return Operation::Left;
+    } else {
+        if let Some(t) = cr.target {
+            for i in 0..s.n {
+                if s.field[i][0] == Some(t) {
+                    if i == cr.y {
+                        return Operation::Grab;
+                    } else if cr.y < i {
+                        return Operation::Down;
+                    } else {
+                        return Operation::Up;
+                    }
+                }
+            }
+            panic!("target not found");
+        } else {
+            return Operation::Nop;
+        }
+    }
+}
+
+fn op_center(s: &State) -> Operation {
+    let cr = s.small_cranes[0].unwrap();
+    if let Some(g) = cr.grabbing {
+       if cr.x < 2 {
+           return Operation::Right;
+       } else if cr.x == 3 {
+           return Operation::Release;
+       } else {
+           let mut d = 10;
+           let mut y = 0;
+           for i in empty_rows(s, 3).iter().copied() {
+               let dd = diff(cr.y, i);
+               if dd < d {
+                   d = dd;
+                   y = i;
+               }
+           }
+           if d == 10 {
+               return Operation::Nop;
+           } else if cr.y < y {
+               return Operation::Down;
+           } else if cr.y > y {
+               return Operation::Up;
+           } else {
+               return Operation::Right;
+           }
+       }
+    } else if cr.x > 2 {
+        return Operation::Left;
+    } else if cr.x < 2 {
+        if let Some(t) = cr.target {
+            if s.field[cr.y][1] == Some(t) {
+                return Operation::Grab;
+            } else {
+                return Operation::Right;
+            }
+        } else {
+            return Operation::Right;
+        }
+    } else {
+        if let Some(t) = cr.target {
+            for i in 0..s.n {
+                if s.field[i][1] == Some(t) {
+                    if i == cr.y {
+                        return Operation::Left;
+                    } else if cr.y < i {
+                        return Operation::Down;
+                    } else {
+                        return Operation::Up;
+                    }
+                }
+            }
+            panic!("target not found");
+        } else {
+            return Operation::Nop;
+        }
+    }
+}
+
+fn op_right(s: &State) -> Operation {
+    let cr = s.small_cranes[1].unwrap();
+    if let Some(g) = cr.grabbing {
+       if cr.x < 4 {
+           return Operation::Right;
+       } else {
+           let y = s.expected.iter().position(|e| *e == Some(g)).unwrap();
+            if cr.y < y {
+               return Operation::Down;
+           } else if cr.y > y {
+               return Operation::Up;
+           } else {
+               return Operation::Release;
+           }
+       }
+    } else if cr.x < 4 {
+        if let Some(t) = cr.target {
+            if s.field[cr.y][3] == Some(t) {
+                return Operation::Grab;
+            } else {
+                return Operation::Right;
+            }
+        } else {
+            return Operation::Right;
+        }
+    } else {
+        if let Some(t) = cr.target {
+            for i in 0..s.n {
+                if s.field[i][3] == Some(t) {
+                    if i == cr.y {
+                        return Operation::Left;
+                    } else if cr.y < i {
+                        return Operation::Down;
+                    } else {
+                        return Operation::Up;
+                    }
+                }
+            }
+            panic!("target not found");
+        } else {
+            return Operation::Nop;
+        }
+    }
+}
+
+fn target_big(s: &State) -> Option<usize> {
+    let mut l = 10_000_000;
+    let mut t = None;
+    let mut ok = false;
+    for i in 0..s.n {
+        if s.field[i][1] == None {
+            ok = true;
+            break;
+        }
+        if s.field[i][0] == None {
+            ok = true;
+            break;
+        }
+    }
+    if !ok {
+        return None;
+    }
+    for e in s.expected_left().iter().copied() {
+        for i in 0..s.n {
+            if s.field[i][0] == Some(e) {
+                let loss = diff(s.big_crane.y, i);
+                if loss < l {
+                    l = loss;
+                    t = Some(e);
+                }
+            }
+        }
+        if l < 10 {
+            return t;
+        }
+        for i in 0..s.n {
+            let n = s.awaiting[i].len();
+            for j in 0..n {
+                if s.awaiting[i][j] == e {
+                    let mut loss = diff(s.big_crane.y, i) + 5;
+                    for k in j..n {
+                        loss += 2;
+                        if next_expected(e, s.n) != Some(s.awaiting[i][k]) {
+                            loss += 10;
+                        }
+                    }
+                    if loss < l {
+                        l = loss;
+                        t = s.field[i][0];
+                    }
+                    break;
+                }
+            }
+        }
+    }
+    return t;
+}
+
+fn empty_rows(s: &State, x: usize) -> Vec<usize> {
+    let mut v = Vec::new();
+    for i in 0..s.n {
+        if s.field[i][x] == None {
+            v.push(i);
+        }
+    }
+    return v;
+}
+
+
+fn target_center(s: &State) -> Option<usize> {
+    let mut l = 10_000_000;
+    let mut t = None;
+    let cr = s.small_cranes[0].unwrap();
+    for e in s.expected_center().iter() {
+        for i in 0..s.n {
+            if s.field[i][1] == Some(*e) {
+                let loss = diff(cr.y, i);
+                if loss < l {
+                    l = loss;
+                    t = Some(*e);
+                }
+            }
+        }
+    }
+    if t != None {
+        return t;
+    }
+    if empty_rows(s, 3).len() <= 1 {
+        return None;
+    }
+    if empty_rows(s, 1).len() > 3 {
+        return None;
+    }
+    for i in 0..s.n {
+        let c = match s.field[i][1] {
+            Some(x) => x,
+            None => continue,
+        };
+        let loss = diff(cr.y, i);
+        if loss < l {
+            l = loss;
+            t = Some(c);
+        }
+    }
+
+    return t;
+}
+
+fn target_right(s: &State) -> Option<usize> {
+    let mut l = 10_000_000;
+    let mut t = None;
+    for oe in s.expected.iter() {
+        let e = match oe {
+            Some(x) => x,
+            None => continue,
+        };
+        for i in 0..s.n {
+            if s.field[i][3] == Some(*e) {
+                let loss = diff(s.small_cranes[1].unwrap().y, i);
+                if loss < l {
+                    l = loss;
+                    t = Some(e);
+                }
+            }
+        }
+    }
+    return t.copied();
 }
 
 
@@ -156,12 +516,18 @@ impl State {
             field: vec![vec![None; n]; n],
             awaiting: e,
             expected: (0..n).map(|i| { Some(n * i) }).collect(),
-            big_crane: Crane {x: 0, y: 0, grabbing: None},
+            big_crane: Crane {
+                x: 0,
+                y: 0,
+                grabbing: None,
+                target: None,
+            },
             small_cranes: (1..n).map(|i| {
                 Some(Crane {
                     x: 0,
                     y: i,
                     grabbing: None,
+                    target: None,
                 })
             }).collect(),
         }
@@ -177,7 +543,13 @@ impl State {
             }
             println!();
         }
-        println!("big: {:?}", self.big_crane.grabbing);
+        println!("big: {:?}-{:?}-{:?}", self.big_crane.grabbing, self.big_crane.target, (self.big_crane.x, self.big_crane.y));
+        if let Some(sc) = self.small_cranes[0] {
+            println!("center: {:?}-{:?}-{:?}", sc.grabbing, sc.target, (sc.x, sc.y));
+        };
+        if let Some(sr) = self.small_cranes[1] {
+            println!("right: {:?}-{:?}-{:?}", sr.grabbing, sr.target, (sr.x, sr.y));
+        };
         for i in 0..self.expected.len() {
             print!("{} ", match self.expected[i] {
                 Some(x) => x.to_string(),
@@ -185,6 +557,57 @@ impl State {
             });
         }
         println!();
+    }
+    
+    fn expected_center(&self) -> Vec<usize> {
+        let mut v = Vec::new();
+        for i in 0..self.expected.len() {
+            if let Some(x) = self.expected[i] {
+                v.push(x);
+            }
+        }
+        for i in 0..v.len() {
+            for _ in 0..3 {
+                let nx = match next_expected(v[i], self.n) {
+                    Some(x) => x,
+                    None => break,
+                };
+                for j in 0..self.n {
+                    let f = match self.field[j][3] {
+                        Some(x) => x,
+                        None => continue,
+                    };
+                    if f == nx {
+                        v.push(f);
+                        break;
+                    } 
+                }
+            }
+        }
+        return v;
+    }
+    
+    fn expected_left(&self) -> Vec<usize> {
+        let mut v = self.expected_center();
+        for i in 0..v.len() {
+            for _ in 0..3 {
+                let nx = match next_expected(v[i], self.n) {
+                    Some(x) => x,
+                    None => break,
+                };
+                for j in 0..self.n {
+                    let f = match self.field[j][1] {
+                        Some(x) => x,
+                        None => continue,
+                    };
+                    if f == nx {
+                        v.push(f);
+                        break;
+                    } 
+                }
+            }
+        }
+        return v;
     }
     
     fn step_big(&mut self, op: &Operation) {
@@ -195,27 +618,28 @@ impl State {
     
     fn step(&mut self, ops: &Vec<Operation>) {
         let bo = ops[0];
-        let (y, x) = (self.big_crane.x, self.big_crane.y);
+        let (x, y) = (self.big_crane.x, self.big_crane.y);
         match bo {
             Operation::Grab => {
-                self.big_crane.grabbing = self.field[x][y];
-                self.field[x][y] = None;
+                self.big_crane.grabbing = self.field[y][x];
+                self.big_crane.target = None;
+                self.field[y][x] = None;
             },
             Operation::Release => {
-                self.field[x][y] = self.big_crane.grabbing;
+                self.field[y][x] = self.big_crane.grabbing;
                 self.big_crane.grabbing = None;
             },
             Operation::Up => {
-                self.big_crane.y = self.big_crane.y - 1;
+                self.big_crane.y = y - 1;
             },
             Operation::Down => {
-                self.big_crane.y = self.big_crane.y + 1;
+                self.big_crane.y = y + 1;
             },
             Operation::Left => {
-                self.big_crane.x = self.big_crane.x - 1;
+                self.big_crane.x = x - 1;
             },
             Operation::Right => {
-                self.big_crane.x = self.big_crane.x + 1;
+                self.big_crane.x = x + 1;
             },
             Operation::Nop => {},
             _ => {},
@@ -230,11 +654,12 @@ impl State {
             let (x, y) = (cr.x, cr.y);
             match so {
                 Operation::Grab => {
-                    cr.grabbing = self.field[x][y];
-                    self.field[x][y] = None;
+                    cr.grabbing = self.field[y][x];
+                    cr.target = None;
+                    self.field[y][x] = None;
                 },
                 Operation::Release => {
-                    self.field[x][y] = cr.grabbing;
+                    self.field[y][x] = cr.grabbing;
                     cr.grabbing = None;
                 },
                 Operation::Up => {
@@ -273,11 +698,7 @@ impl State {
                 }
                 let r = self.field[i].len() - 1;
                 self.field[i][r] = None;
-                if d+1 < self.n * (i+1) {
-                    self.expected[i] = Some(d + 1);
-                } else {
-                    self.expected[i] = None;
-                }
+                self.expected[i] = next_expected(d, self.n);
             }
         }
         
@@ -288,10 +709,27 @@ impl State {
     }
 }
 
+fn next_expected(c: usize, n: usize) -> Option<usize> {
+    let cd = c + 1;
+    if cd % n == 0 {
+        return None;
+    }
+    return Some(cd);
+}
+
+fn diff(a: usize, b: usize) -> usize {
+    if a > b {
+        return a - b;
+    }
+    return b - a;
+}
+
+#[derive(Clone, Copy)]
 struct Crane {
     x: usize,
     y: usize,
     grabbing: Option<usize>,
+    target: Option<usize>,
 }
 
 impl Crane {
@@ -300,7 +738,7 @@ impl Crane {
     }
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, PartialEq)]
 enum Operation {
     Grab,
     Release,
