3bbcfd904091253c8a415b26d5b173a33e621b09
seiya <20365512+seiyab@users.noreply.github.com>
2024-08-31 09:44:37 +0900

    enhance panic checker

--- AHC/03x/036/eval.py
+++ AHC/03x/036/eval.py
@@ -4,7 +4,8 @@ from pathlib import Path
 def main():
     my_env = os.environ.copy()
     check_panic = my_env.get("PANIC", "") == "1"
-    my_env["SCORE"] = "1"
+    if not check_panic:
+        my_env["SCORE"] = "1"
     subprocess.run(["cargo", "build", "--release", "--offline"], check=True)
     score = 0
     worst_time = 0
@@ -21,14 +22,20 @@ def main():
         end = time.time()
         elapsed = int((end - start) * 1000)
         out = o.stdout
-        local_score = int(out)
-        if i < 15:
-            _n, _m, _t, la, lb = d.split("\n")[0].split(" ")
-            print(f"{p.name} ({int(la):4d}, {int(lb):2d}): {local_score}, {elapsed}")
-        score += local_score
-        worst_time = max(worst_time, elapsed)
-    print("--------------------")
-    print(f"Average: {int(score / len(fs))}")
-    print(f"Worst time: {worst_time} ms")
+        _n, _m, _t, la, lb = d.split("\n")[0].split(" ")
+        if check_panic:
+            outl = len(out.split("\n")[0].split(" "))
+            if outl != int(la):
+                raise Exception(f"Invalid input: {p.name}, ({outl}, {la})")
+        else:
+            local_score = int(out)
+            if i < 15 and not check_panic:
+                print(f"{p.name} ({int(la):4d}, {int(lb):2d}): {local_score}, {elapsed}")
+            score += local_score
+            worst_time = max(worst_time, elapsed)
+    if not check_panic:
+        print("--------------------")
+        print(f"Average: {int(score / len(fs))}")
+        print(f"Worst time: {worst_time} ms")
 
 main()
